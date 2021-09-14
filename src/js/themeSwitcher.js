692b5d264b2f6e219868cb3235962c1ba1c776df
Alvaro Rumpel <alvaroRumpel@gmail.com>
2021-09-14 01:28:00 -0300

    os bagulho tudo

---
+++ src/js/themeSwitcher.js
@@ -0,0 +1,13 @@
+function switchTheme(e){
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+toggleSwitch = document.getElementById('checkbox');
+
+toggleSwitch.addEventListener('change', switchTheme, false);
